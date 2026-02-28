2ac1b1e5f58396286267315e7620668afacea840
Joud Aljadani <Joudalimab@gmail.com>
2026-03-01 02:52:57 +0300

    Changes With comments

--- src/surpriseCinema/app/Appframe.java
+++ src/surpriseCinema/app/Appframe.java
@@ -7,6 +7,7 @@ import java.awt.*;
 
 public class Appframe extends JFrame {
 
+    //Card names for navigation between pages
     public static final String SPLASH = "SPLASH";
     public static final String SIGNUP = "SIGNUP";
     public static final String SIGNIN = "SIGNIN";
@@ -16,16 +17,20 @@ public class Appframe extends JFrame {
     private final JPanel root = new JPanel(layout);
 
     public Appframe() {
+
+        //Window Settings
         setTitle("Surprise Cinema");
         setSize(390, 720);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        //Add all pages and link it with card names
         root.add(new Splash(this), SPLASH);
         root.add(new SignUp(this), SIGNUP);
         root.add(new SignIn(this), SIGNIN);
         root.add(new Preferences(this), PREFERENCES);
 
+        //Use root panel as the main container of this frame
         setContentPane(root);
         showPage(SPLASH);
     }
@@ -46,6 +51,7 @@ public class Appframe extends JFrame {
                     break;
             }
 
-            layout.show(root, page);
+        //Display this page using CardLayout
+        layout.show(root, page);
         }
     }
