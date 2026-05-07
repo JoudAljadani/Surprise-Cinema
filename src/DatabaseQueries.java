e2df786ff50a32fc8048946321e497049653f6e6
Jana <itsjana2@gmail.com>
2026-05-07 10:18:09 +0300

    update DatabseQueries class

--- src/DatabaseQueries.java
+++ src/DatabaseQueries.java
@@ -1,5 +1,5 @@
 import java.sql.*;
-public class UserQueries {
+public class DatabaseQueries {
 
     // Add new user to USERS table
     public static void addUser(User user) {
@@ -91,5 +91,72 @@ public class UserQueries {
             System.out.println(e.getMessage());
         }
         return false;
+    }
+
+    public static User getUserByLogin(String email, String password) {
+
+        Connection con = null;
+
+        try {
+            con = DatabaseManager.connect();
+            Statement st = con.createStatement();
+
+            String sql =
+                    "SELECT * FROM USERS " +
+                    "WHERE EMAIL = '" + email + "' " +
+                    "AND PASSWORD = '" + password + "'";
+            ResultSet rs = st.executeQuery(sql);
+            if (rs.next()) {
+                String name = rs.getString("NAME");
+                String userEmail = rs.getString("EMAIL");
+                String userPassword = rs.getString("PASSWORD");
+                int age = rs.getInt("AGE");
+                String gender = rs.getString("GENDER");
+                con.close();
+                return new User(name, userEmail, userPassword, age, gender);
+            }
+            con.close();
+        } catch (SQLException e) {
+            System.out.println("Get user login error!");
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+    // Add new ticket to TICKETS table
+    public static void addTicket(Ticket ticket) {
+
+        Connection con = null;
+        try {
+
+            // (1) Create connection
+            con = DatabaseManager.connect();
+
+            // (2) Create statement object
+            Statement st = con.createStatement();
+
+            // (3) Execute SQL statement
+            String sql =
+                    "INSERT INTO TICKETS " +
+                            "(MOVIE_NAME, CINEMA_NAME, HALL, SHOW_DATE, SHOW_TIME, SEAT, USER_EMAIL) " +
+                            "VALUES (" +
+                            "'" + ticket.getMovieName() + "', " +
+                            "'" + ticket.getCinemaName() + "', " +
+                            "'" + ticket.getHall() + "', " +
+                            "'" + ticket.getDate() + "', " +
+                            "'" + ticket.getShowTime() + "', " +
+                            "'" + ticket.getSeat() + "', " +
+                            "'" + ticket.getUserEmail() + "')";
+            st.executeUpdate(sql);
+
+            System.out.println("Ticket added successfully");
+            // (4) Close connection
+            con.close();
+        } catch (SQLException e) {
+
+            System.out.println("Ticket insert error!");
+
+            System.out.println(e.getMessage());
+        }
     }
 }
